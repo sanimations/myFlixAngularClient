f4ba638dd4c8ef293e91d3c0f094b338a7ec9471
sanimations <smartucci157@gmail.com>
2024-01-20 17:03:39 -0500

    Updated userregistration to FetchApiData and created user-login-form component

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -9,7 +9,8 @@ const apiUrl = 'https://queer-films-a4556bef0856.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
+
+export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
   // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -7,32 +7,32 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-
 @Component({
-  selector: 'app-user-registration-form',
-  templateUrl: './user-registration-form.component.html',
-  styleUrls: ['./user-registration-form.component.scss']
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrl: './user-login-form.component.scss'
 })
-export class UserRegistrationFormComponent implements OnInit {
+export class UserLoginFormComponent implements OnInit {
+  
+  @Input() loginData = { Username: '', Password: ''}
 
-  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
-
-constructor(
+  constructor(
     public fetchApiData: FetchApiDataService,
-    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar) { }
 
 ngOnInit(): void {
 }
 
-// This is the function responsible for sending the form inputs to the backend
-registerUser(): void {
-    this.fetchApiData.userRegistration(this.userData).subscribe({
+loginUser(): void {
+    this.fetchApiData.userLogin(this.loginData).subscribe({
       next: (result: any) => {
+        localStorage.setItem('user',  JSON.stringify(result.user));
+        localStorage.setItem('token', result.token);
   // Logic for a successful user registration goes here! (To be implemented)
      this.dialogRef.close(); // This will close the modal on success!
      console.log(result)
-     this.snackBar.open(result, 'OK', {
+     this.snackBar.open('Login Successful', 'OK', {
         duration: 2000
      });
     }, 

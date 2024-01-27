3d76d0e2150fb437bbb0c67e250057f9978cf1ec
sanimations <smartucci157@gmail.com>
2024-01-27 18:15:47 -0500

    Updated movie-card with router to profile and adjusted the getMovies to show Favorited Movies

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,38 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service'
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit{
 
   movies: any[] = [];
-  
-  constructor(public fetchApiData: FetchApiDataService) { }
 
-ngOnInit(): void {
-  this.getMovies();
-}
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    private router: Router
+  ) { }
 
-getMovies(): void {
-  this.fetchApiData.getAllMovies().subscribe((resp: any) => {
-      this.movies = resp;
-      console.log(this.movies);
-      return this.movies;
-    });
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies(): void {
+    const u = localStorage.getItem('username');
+    if (u) {
+      this.fetchApiData.getAllMovies().subscribe((res: any) => {
+        this.movies = res;
+        // Fetch user's favorite movies
+        this.fetchApiData.getUserFavoriteMovies(u).subscribe((favorites: any[]) => {
+          // Iterate through each movie and check if it's in the user's favorites
+          this.movies.forEach((movie: any) => {
+            movie.isFavorite = favorites.some((fav: any) => fav._id === movie._id);
+          });
+        });
+      });
+    } else {
+      console.error('Username not found in localStorage');
+    }
+  }
+
+  getUserProfile(): void {
+    this.router.navigate(['profile']);
   }
 
   toggleFavorite(movie: any) {
-    const u = localStorage.getItem('user');
+    const u = localStorage.getItem('username');
     if (u) {
       if (movie.isFavorite) {
-        this.fetchApiData.deleteFavoriteMovie(u, movie.id).subscribe(() => {
+        this.fetchApiData.deleteFavoriteMovie(u, movie._id).subscribe(() => {
           movie.isFavorite = false;
         });
       } else {
-        this.fetchApiData.addFavoriteMovie(u, movie.id).subscribe(() => {
+        this.fetchApiData.addFavoriteMovie(u, movie._id).subscribe(() => {
           movie.isFavorite = true;
         });
       }

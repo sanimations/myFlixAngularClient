443f5c2a84538ce28e5a6ac66bc77b7e446fd9a2
sanimations <smartucci157@gmail.com>
2024-01-28 20:47:11 -0500

    Added a genre-card

---
+++ src/app/genre-card/genre-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-genre-card',
+  templateUrl: './genre-card.component.html',
+  styleUrls: ['./genre-card.component.scss']
+})
+export class GenreCardComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  
+}
+

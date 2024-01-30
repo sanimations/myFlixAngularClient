0b32df8a231be6bd9f876d8e06286740c46b4b19
sanimations <smartucci157@gmail.com>
2024-01-29 23:50:53 -0500

    Added login to the user registration

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -7,6 +7,8 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-user-registration-form',
@@ -17,32 +19,48 @@ export class UserRegistrationFormComponent implements OnInit {
 
   @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
-constructor(
+  constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
-    public snackBar: MatSnackBar) { }
+    public snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
-ngOnInit(): void {
-}
+  ngOnInit(): void {
+  }
 
-// This is the function responsible for sending the form inputs to the backend
-registerUser(): void {
+  // This is the function responsible for sending the form inputs to the backend
+  registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe({
       next: (result: any) => {
-  // Logic for a successful user registration goes here! (To be implemented)
-     this.dialogRef.close(); // This will close the modal on success!
-     console.log(result)
-     this.snackBar.open(result, 'OK', {
-        duration: 2000
-     });
-    }, 
-    error: (error: any) => {
-      console.log(error);
-      this.snackBar.open(error, 'OK', {
-        duration: 2000
-      });
-    }
+        let loginData = { username: this.userData.Username, password: this.userData.Password }
+        this.fetchApiData.userLogin(loginData).subscribe({
+          next: (loginResult: any) => {
+            localStorage.setItem('user', JSON.stringify(result.user));
+            localStorage.setItem('email', loginResult.user.Email);
+            localStorage.setItem('birthday', loginResult.user.Birthday);
+            localStorage.setItem('username', loginResult.user.Username);
+            localStorage.setItem('password', loginResult.user.Password);
+            localStorage.setItem('token', loginResult.token);
+            this.dialogRef.close();
+            this.router.navigate(['movies']);
+          }
+        });
+        console.log('signup/login worked');
+
+        // Logic for a successful user registration goes here! (To be implemented)
+        console.log(result)
+        this.snackBar.open('Welcome to Queer Cinema!', 'OK', {
+          duration: 2000
+        });
+      },
+      error: (error: any) => {
+        console.log(error);
+        this.snackBar.open(error, 'OK', {
+          duration: 2000
+        });
+      }
     });
   }
 
-  }
+}

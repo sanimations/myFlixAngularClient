f2b909b4a290e0c083f50f1aee9bd68ab353b05a
sanimations <smartucci157@gmail.com>
2024-01-29 23:52:30 -0500

    Updated User profile page to show favorite movies, needs fixing

--- src/app/user-profile-page/user-profile-page.component.ts
+++ src/app/user-profile-page/user-profile-page.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FetchApiDataService } from '../fetch-api-data.service'
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { map } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import { EditProfileComponent } from '../edit-profile/edit-profile.component';
+import { GenreCardComponent } from '../genre-card/genre-card.component';
+import { DirectorCardComponent } from '../director-card/director-card.component';
+import { DescriptionCardComponent } from '../description-card/description-card.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { Router } from '@angular/router';
 
@@ -10,14 +15,15 @@ import { Router } from '@angular/router';
   templateUrl: './user-profile-page.component.html',
   styleUrl: './user-profile-page.component.scss'
 })
-export class UserProfilePageComponent implements OnInit{
+export class UserProfilePageComponent implements OnInit {
 
-  user: any = {Username: '', Password: '', Email: '', Birthday: '' };
+  user: any = { Username: '', Password: '', Email: '', Birthday: '' };
   favoriteMovies: any[] = [];
 
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialog: MatDialog,
+    public snackBar: MatSnackBar,
     private router: Router
   ) { }
 
@@ -30,22 +36,73 @@ export class UserProfilePageComponent implements OnInit{
     };
   }
 
+
   openEditProfileDialog(): void {
-      this.dialog.open(EditProfileComponent, {
-        width: '280px'
-      });
+    this.dialog.open(EditProfileComponent, {
+      width: '280px'
+    });
   }
 
   deleteProfile(): void {
-
+    let username = localStorage.getItem('username');
+    if (username) {
+      this.fetchApiData.deleteUser(username).subscribe({
+        next: (response) => {
+          localStorage.clear();
+          this.router.navigate(['welcome']);
+          console.log('User deleted successfully:', response);
+          this.snackBar.open('Bye now!', 'Close', {
+            duration: 3000, 
+          });
+        },
+        error: (error) => {
+          console.error('Error deleting user:', error);
+          // Optionally, handle the error scenario (e.g., display an error message)
+        }
+      });
+    } else {
+      console.log('Username not found!');
+    }
   }
 
-  logout(): void{
+  openGenreDialog(Genre: any): void {
+    this.dialog.open(GenreCardComponent, {
+      data: {
+       name: Genre.Name,
+       description: Genre.Description,
+       
+      }
+    })
+    };
+  
+
+    openDirectorDialog(Director: any): void {
+      this.dialog.open(DirectorCardComponent, {
+        data: {
+          name: Director.Name,
+          bio: Director.Description,
+          birth: Director.Birth
+          
+         }
+      })
+    }
+
+    openDescriptionDialog(Description: any, Genre: any, Director: any): void {
+      this.dialog.open(DescriptionCardComponent, {
+        data: {
+          description: Description,
+          director: Director.Name,
+          genre: Genre.Name
+        }
+      });
+    }
+
+  logout(): void {
     localStorage.clear(),
-    this.router.navigate(['welcome']);
+      this.router.navigate(['welcome']);
   }
-  back(): void{
-  this.router.navigate(['movies']);
+  back(): void {
+    this.router.navigate(['movies']);
   }
 
 }
